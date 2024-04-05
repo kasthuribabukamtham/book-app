af8ca6357658bbb1c10af4b28e31036d7b7bcdc0
Babu <kasthuribabu8465@users.noreply.github.com>
2024-04-05 00:40:37 -0700

    second commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
 
+interface Note {
+  content: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  notes: Note[] = [];
 
-  constructor() {}
+  ionViewWillEnter() {
+    const storedNotes = localStorage.getItem('notes');
+    if (storedNotes) {
+      this.notes = JSON.parse(storedNotes);
+    }
+  }
 
+  saveNotes() {
+    localStorage.setItem('notes', JSON.stringify(this.notes));
+  }
+
+  addNote() {
+    this.notes.push({ content: '' });
+  }
+
+  deleteNote(note: Note) {
+    const index = this.notes.indexOf(note);
+    if (index > -1) {
+      this.notes.splice(index, 1);
+      this.saveNotes();
+    }
+  }
 }
